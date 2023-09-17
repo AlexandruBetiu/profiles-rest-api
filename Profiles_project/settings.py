ddc899b3fad0b27cfd2616d625b9943f04b40774
Alex <alexandrubetiu@gmail.com>
2023-09-17 16:22:52 +0300

    pus migratii

--- Profiles_project/settings.py
+++ Profiles_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framewrk.authtoken'
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "profiles_api.UserProfile"
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin, BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self,email,name,password=None):
+        """Creaza un nou user profile"""
+        if not email:
+            raise ValueError('Baga un email coaie')
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, name = name)
+        
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,name,password):
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=100,unique=True)
@@ -8,7 +36,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default= False)
 
-    objects = UserPrifileManager()
+    objects = UserProfileManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['name']
